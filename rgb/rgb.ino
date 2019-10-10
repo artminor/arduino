a9ee1598b1320456fe9192e6f3dab88e47b6bc9c
Jun Chen <junchen07@gmail.com>
2019-10-09 23:56:58 -0400

    rgb led

---
+++ rgb/rgb.ino
@@ -0,0 +1,19 @@
+/*
+Adafruit Arduino - Lesson 3. RGB LED
+*/
+ 
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
+ 
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
